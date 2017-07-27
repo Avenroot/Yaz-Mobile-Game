a81c42679e7859789db1212ccac27aa29fbdfdc8
Dana Dill <dana@twonkd.com>
2017-07-27 23:16:18 +0200

    Layout updates

---
+++ Source/Yaz for Android/UIDiceComponent.swift
@@ -0,0 +1,27 @@
+﻿import java.util
+import java.lang
+
+public class UIDiceComponent {
+
+	var diceImage1 = R.drawable.dice1
+	var diceImage2 = R.drawable.dice2
+	var diceImage3 = R.drawable.dice3
+	var diceImage4 = R.drawable.dice4
+	var diceImage5 = R.drawable.dice5
+	var diceImage6 = R.drawable.dice6
+
+	var YazTimer = Timer()
+
+
+	func RollDice() {
+
+		// update image
+		// update the value
+
+	}
+
+	func StopDice() {
+
+	}
+
+}
