7c048bd666f8980e6fa4b8b4239c99ae1c9e85af
Dana Dill <dana.dill@gmail.com>
2018-04-16 11:32:40 +0200

    small updates
    * updated maifest to org.fsxp.yazandroid
    * refactored some code

--- Source/Yaz for Android/yazDice.swift
+++ Source/Yaz for Android/yazDice.swift
@@ -16,37 +16,44 @@
         if !locked {
 
             let rand = Random()       
-            var r = rand.NextInt(6) + 1   
+            var r = rand.NextInt(6)   
             value = r    
+            
+            switch r {
+
+                case 0: 
+                    let drawableID = Context.getResources().getIdentifier("dice1.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
                 
-            if r == 1 {
-                let drawableID = Context.getResources().getIdentifier("dice1.jpg", "drawable", Context.getPackageName())
-                self.setImageResource(drawableID)
-            }
-                           
-            if r == 2 {
-                let drawableID = Context.getResources().getIdentifier("dice2.jpg", "drawable", Context.getPackageName())
-                self.setImageResource(drawableID)
-            }
 
-            if r == 3 {
-                let drawableID = Context.getResources().getIdentifier("dice3.jpg", "drawable", Context.getPackageName())
-                self.setImageResource(drawableID)
-            }
-                           
-            if r == 4 {
-                let drawableID = Context.getResources().getIdentifier("dice4.jpg", "drawable", Context.getPackageName())
-                self.setImageResource(drawableID)
-            }
+                case 1: 
+                    let drawableID = Context.getResources().getIdentifier("dice2.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
+                
 
-            if r == 5 {
-                let drawableID = Context.getResources().getIdentifier("dice5.jpg", "drawable", Context.getPackageName())
-                self.setImageResource(drawableID)
-            }
-                           
-            if r == 6 {
-                let drawableID = Context.getResources().getIdentifier("dice6.jpg", "drawable", Context.getPackageName())
-                self.setImageResource(drawableID)
+                case 2: 
+                    let drawableID = Context.getResources().getIdentifier("dice3.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
+                
+
+                case 3: 
+                    let drawableID = Context.getResources().getIdentifier("dice4.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
+                
+
+                case 4: 
+                    let drawableID = Context.getResources().getIdentifier("dice5.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
+                
+
+                case 5: 
+                    let drawableID = Context.getResources().getIdentifier("dice6.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
+
+                default:  
+                    let drawableID = Context.getResources().getIdentifier("dice1.jpg", "drawable", Context.getPackageName())
+                    self.setImageResource(drawableID)
+                
             }
 
         }
