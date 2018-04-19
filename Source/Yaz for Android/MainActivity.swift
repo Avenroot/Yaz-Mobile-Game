e08df74f50de3ecd3e80d19ed7f2f5fdd622b1f7
Łukasz Milunas <milunas.lukasz@gmail.com>
2018-04-19 11:44:30 +0200

    change forced type conversion

--- Source/Yaz for Android/MainActivity.swift
+++ Source/Yaz for Android/MainActivity.swift
@@ -12,14 +12,14 @@ public class MainActivity: Activity {
     super.onCreate(savedInstanceState)
     ContentView = R.layout.main
 
-    let diceOne = self.findViewById(R.id.ivDiceOne) as! yazDice
-    let diceTwo = self.findViewById(R.id.ivDiceTwo) as! yazDice
-    let diceThree = self.findViewById(R.id.ivDiceThree) as! yazDice
-    let diceFour = self.findViewById(R.id.ivDiceFour) as! yazDice
-    let diceFive = self.findViewById(R.id.ivDiceFive) as! yazDice
+    let diceOne = self.findViewById(R.id.ivDiceOne) as yazDice
+    let diceTwo = self.findViewById(R.id.ivDiceTwo) as yazDice
+    let diceThree = self.findViewById(R.id.ivDiceThree) as yazDice
+    let diceFour = self.findViewById(R.id.ivDiceFour) as yazDice
+    let diceFive = self.findViewById(R.id.ivDiceFive) as yazDice
 
 
-    let btnones = findViewById(R.id.btnRollDice) as! Button   
+    let btnones = findViewById(R.id.btnRollDice) as! Button
 
     btnones.OnClickListener = { (v: View!) in
 
